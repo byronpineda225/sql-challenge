676e0f63e7c46fb50c0a7f1b4f6a803fe18fef4b
byronpineda225 <84112237+byronpineda225@users.noreply.github.com>
2021-08-01 22:32:58 -0400

    Adding schema SQL

---
+++ EmployeeSQL/Schema_tables.sql
@@ -0,0 +1,41 @@
+-- Author: Byron Pineda
+-- Database name: sql_challenge_db
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(30) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(30) NOT NULL,
+    hire_date DATE NOT NULL
+);
+
+CREATE TABLE salaries (
+    emp_no INT  PRIMARY KEY,
+    salary INT  NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(30) PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(30),
+    emp_no INT NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(30) PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
+);
